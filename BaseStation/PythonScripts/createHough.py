9df243b31c4d54bc7dd6f7abdb1a9c49e1db575f
Oscar <oscarrimsby@gmail.com>
2019-12-09 11:48:08 +0100

    Added creation of two different hough spectrums to createHough.py

--- BaseStation/PythonScripts/createHough.py
+++ BaseStation/PythonScripts/createHough.py
@@ -1,8 +1,9 @@
 from math import hypot, pi, cos, sin
 import numpy as np
 from PIL import Image
+import cv2
 
-def hough(im, ntx=2000, mry=1600):
+def hough(im, ntx=850, mry=480):
     """
     Calculate Hough transform.
 
@@ -22,16 +23,70 @@ def hough(im, ntx=2000, mry=1600):
     for jx in xrange(nimx):
         for iy in xrange(mimy):
             col = pim[jx, iy]
-            if col == 255: continue
+            if col == 0: continue
             for jtx in xrange(ntx):
                 th = dth * jtx
+                th = th - (pi/2)
                 r = jx*cos(th) + iy*sin(th)
                 iry = mry/2 + int(r/dr+0.5)
                 phim[jtx, iry] -= 1
     return him
 
+def detectEdges(img, savePath):
+	edges = cv2.Canny(img,100,200,True)
+	cv2.imwrite(savePath, edges)
 
-im = Image.open('../Images/map2.pgm').convert('L') #Loads the image and makes it grayscale
+def avg(im):
+	pim = im.load()
+	imx, imy = im.size
+	nim = Image.new("L", (imx, 256), 255)
+	pnim = nim.load()
+	for x in range(imx):
+		i = 0
+		tot = 0
+		for y in range(imy):
+			if pim[x, y] != 255:
+				i += 1
+				#print x
+				#print y
+				#print "\n"
+				tot -= pim[x,y]
+		pnim[x, tot/i] = 0
+	return nim
+
+def tIFmax(im):
+	pim = im.load()
+	imx, imy = im.size
+	nim = Image.new("L", (imx, 256), 255)
+	pnim = nim.load()
+	for x in range(imx):
+	    max = 255
+	    for y in range(imy):
+	    	if pim[x, y] != 255 and max > pim[x,y]:
+	    		max = pim[x,y]
+	    pnim[x, max] = 0
+	return nim
+
+img = cv2.imread('../Images/map.pgm')
+img2 = cv2.imread('../Images/map2.pgm')
+
+detectEdges(img, '../Images/edgesH.png')
+detectEdges(img2, '../Images/edges2H.png')
+
+im = Image.open('../Images/edgesH.png').convert('L') #Loads the image and makes it grayscale
 him = hough(im)
-him.save('../Images/ho2.bmp')
+tIF = avg(him)
+tIF.save('../Images/tIF.bmp')
+tIFma = tIFmax(him)
+tIFma.save('../Images/tIFmax.bmp')
+him.save('../Images/ho.bmp')
 im.close()
+
+im2 = Image.open('../Images/edges2H.png').convert('L') #Loads the image and makes it grayscale
+him2 = hough(im2)
+tIF = avg(him2)
+tIF.save('../Images/tIF2.bmp')
+tIFmax2 = tIFmax(him2)
+tIFmax2.save('../Images/tIFmax2.bmp')
+him2.save('../Images/ho2.bmp')
+im2.close()
--- BaseStation/PythonScripts/preProcess.py
+++ BaseStation/PythonScripts/preProcess.py
@@ -25,12 +25,13 @@ def make2houghP(img):
 def make2hough(img):
     """
     Inputs an opened image and returns the result of a hough transform
-    in the (tho,theta)-plane
+    in the (tho,theta)-plane. The points in the output array are the 
+    points of interest derived from the hough space
     """
     #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(img,800,1200,True)
-    cv2.imshow('edges', edges) #Shows the detected edges
-    cv2.waitKey(0)
+    #cv2.imshow('edges', edges) #Shows the detected edges
+    #cv2.waitKey(0)
     return cv2.HoughLines(edges,1,np.pi/90,70)
 
 
@@ -50,20 +51,20 @@ def vizHough(hough, img, saveFile):
 	"""
 	Vizualizes a hough transform on top of an image and is saved
 	"""
-    for lns in hough:
-        for rho,theta in lns:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
+	for lns in hough:
+		for rho,theta in lns:
+			a = np.cos(theta)
+			b = np.sin(theta)
+			x0 = a*rho
+			y0 = b*rho
+			x1 = int(x0 + 1000*(-b))
+			y1 = int(y0 + 1000*(a))
+			x2 = int(x0 - 1000*(-b))
+			y2 = int(y0 - 1000*(a))
 
-            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+			cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-    cv2.imwrite(saveFile,img)
+	cv2.imwrite(saveFile,img)
 
 
 def rotateP(hough, img, saveFile):
@@ -71,40 +72,65 @@ def rotateP(hough, img, saveFile):
 	Rotates a probabilistic hough transform by finding the longest vector and
 	turns it to be horizontal
 	"""
-    largestId = 0
-    largestLen = 0
-    i = 0
-    for lns in hough:
-        for x1,y1,x2,y2 in lns:
-            len = math.sqrt(math.pow(x1-x2,2)+math.pow(y1-y2,2))
-            if len > largestLen:
-                largestId = i
-                largestLen = len
-        i = i+1
-    print largestId
-    print largestLen
+	largestId = 0
+	largestLen = 0
+	i = 0
+	for lns in hough:
+		for x1,y1,x2,y2 in lns:
+			len = math.sqrt(math.pow(x1-x2,2)+math.pow(y1-y2,2))
+			if len > largestLen:
+				largestId = i
+				largestLen = len
+		i = i+1
+	print largestId
+	print largestLen
 
-    angles = []
+	angles = []
 
-    for x1, y1, x2, y2 in hough[largestId]:
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        angles.append(angle)
+	for x1, y1, x2, y2 in hough[largestId]:
+		cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+		angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+		angles.append(angle)
 
-    median_angle = np.median(angles)
-    img_rotated = ndimage.rotate(img, median_angle)
+	median_angle = np.median(angles)
+	img_rotated = ndimage.rotate(img, median_angle)
 
-    print "Angle is {}".format(median_angle)
-    cv2.imwrite(saveFile, img_rotated)
+	print "Angle is {}".format(median_angle)
+	cv2.imwrite(saveFile, img_rotated)
+
+def extendImage(img, larger):
+	size = larger.size
+	layer = Image.new('L', size, 205)
+	layer.paste(img, tuple(map(lambda x:(x[0]-x[1])/2, zip(size, img.size))))
+	layer.save('../Images/extended.pgm')
 
 
+img = Image.open('../Images/map.pgm')
+img2 = Image.open('../Images/map2.pgm')
 
-img = cv2.imread('../Images/map.pgm')
+if img.size > img2.size:
+	extendImage(img2, img)
+	img = cv2.imread('../Images/map.pgm')
+	img2 = cv2.imread('../Images/extended.pgm')
+elif img.size < img2.size:
+	extendImage(img, img2)
+	img = cv2.imread('../Images/extended.pgm')
+	img2 = cv2.imread('../Images/map2.pgm')
+else:
+	img = cv2.imread('../Images/map.pgm')
+	img2 = cv2.imread('../Images/map2.pgm')
+
 lines = make2hough(img)
 vizHough(lines, img, '../Images/hough.jpg')
 #rotateP(lines, img, 'hough_rotate.jpg')
 
-img2 = cv2.imread('../Images/map2.pgm')
 lines2 = make2hough(img2)
+print 'Lines'
+for i in lines:
+	print i
+
+print '\nLines2'
+for i in lines2:
+	print i
 vizHough(lines2, img2, '../Images/hough2.jpg')
 #rotateP(lines2, img2, 'hough2_rotate.jpg')
