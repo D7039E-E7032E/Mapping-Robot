a4a5f75ae294ccc4d7443c000515ca4239bd72a7
anti002 <anti555333@gmail.com>
2019-12-13 10:16:28 +0100

    Changed to 60s sleep

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ rosrun map_server map_saver -f home/vision/Documents/Maps #Saves map to a certai
 scp map.pgm map.yaml coarl@192.168.1.97:~/Documents/Maps #Transfers maps to central computer
 sudo rm ~/Documents/Maps/map.pgm #Removes transfered files
 sudo rm ~/Documents/Maps/map.yaml
-sleep 10 #Waits
+sleep 60 #Waits
 done
