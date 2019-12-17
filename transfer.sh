ee7fd4857eb636d5eac24c172d664c2467123618
anti002 <anti555333@gmail.com>
2019-12-17 09:34:21 +0100

    updated scripts

---
+++ transfer.sh
@@ -0,0 +1,5 @@
+rosrun map_server map_saver -f home/vision/Documents/Maps #Saves map to a certain dir
+scp map.pgm map.yaml coarl@192.168.1.97:~/Documents/Maps #Transfers maps to central computer
+sudo rm ~/Documents/Maps/map.pgm #Removes transfered files
+sudo rm ~/Documents/Maps/map.yaml
+
